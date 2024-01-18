443f18ca8a60bb7c82d8653dd1d4c35ff7bfa7ad
Crush007 <sanya.tumanow2011@yandex.ru>
2024-01-18 06:39:24 +0400

    18.01

--- src/js/modules/btn-question.js
+++ src/js/modules/btn-question.js
@@ -1,7 +1,14 @@
 function btnQuestion() {
-	const btnQAr = document.querySelectorAll('.question__item');
-    btnQAr.forEach(function(item){
+	const question = document.querySelectorAll('.question__item');
+    const answers = document.querySelectorAll('.answer');
+    question.forEach(function(item){
         item.addEventListener('click', function(){
+            answers.forEach(function(item){
+                document.querySelectorAll('.arrow__btn').forEach(function(item){
+                    item.classList.remove('rotate');
+                })
+                item.classList.remove('open');
+            });
             const answer = this.querySelector('.answer');
             const arrow = this.querySelector('.arrow__btn');
             arrow.classList.toggle('rotate');
