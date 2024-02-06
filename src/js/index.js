41d38c75734ca9169230d0a86336de43f467f034
Crush007 <sanya.tumanow2011@yandex.ru>
2024-02-06 14:07:21 +0400

    06.02

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,25 @@
 import mobileNav from './modules/mobile-nav.js';
 import btnQuestion from './modules/btn-question.js';
 
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
+
+
+const swiper = new Swiper('.reviews-carousel', {
+
+    loop: true,
+  
+
+    // pagination: {
+    //   el: '.swiper-pagination',
+    // },
+  
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
 // mobileNav();
 btnQuestion();
